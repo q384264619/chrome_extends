2c13f3dfa07b2d059ac34c9ed0d2bc772037df01
petter.he <1061051373@qq.com>
2026-06-05 10:46:06 +0800

    针对 有任务的情况,添加自动页面刷新,以防止部分页面存在登录失效的情况

--- autoClick/contentScript.js
+++ autoClick/contentScript.js
@@ -1,1 +1,18 @@
-// contentScript.js - 内容脚本，在页面中执行点击操作
+// contentScript.js 
+// 初始化
+refreshPageWhenHaveTask();
+
+async function refreshPageWhenHaveTask() {
+    console.log('[Content]  ', '开始执行检测');
+    const verifyResult = await chrome.storage.sync.get(['tasks']);
+    if (verifyResult.tasks && verifyResult.tasks.length > 0) {
+        console.log('[Content]  Storage中任务数量:', verifyResult.tasks ? verifyResult.tasks.length : 0);
+        const maxOffset = 1000 * 60 * 5;
+        const tomorrowRandomOffset = Math.floor(Math.random() * (maxOffset * 2 + 1)) - maxOffset;
+        console.log('[Content]  ', tomorrowRandomOffset/1000,'秒后刷新页面');
+        setTimeout(() => {
+            console.log('[Content]  ', '正在刷新页面...');
+            window.location.reload();
+        }, tomorrowRandomOffset);
+    }
+}
